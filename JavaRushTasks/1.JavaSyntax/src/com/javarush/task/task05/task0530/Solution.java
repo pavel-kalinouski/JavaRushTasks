2e2aa06438cb705b10dd7fa5c81fe9ac8e7f12ae
pavel-kalinouski <pavelpaune@ya.ru>
2020-03-21 19:14:30 +0300

    task0530

---
+++ JavaRushTasks/1.JavaSyntax/src/com/javarush/task/task05/task0530/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task05.task0530;
+import java.io.*;
+
+/* 
+Шеф, что-то не пашет
+Задача: Программа считывает два числа с клавиатуры и выводит их сумму на экран.
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(reader.readLine());
+        int b = Integer.parseInt(reader.readLine());
+
+        int sum = a + b;
+        System.out.println("Sum = " + sum);
+    }
+}
