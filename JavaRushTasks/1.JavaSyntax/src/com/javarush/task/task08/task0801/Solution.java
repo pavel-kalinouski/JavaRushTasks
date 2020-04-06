289572c01b79c2709f5e2249b6802f385836e442
pavel-kalinouski <pavelpaune@ya.ru>
2020-04-06 17:24:36 +0300

    task0801

---
+++ JavaRushTasks/1.JavaSyntax/src/com/javarush/task/task08/task0801/Solution.java
@@ -0,0 +1,40 @@
+package com.javarush.task.task08.task0801;
+
+/* 
+Set из растений
+Создать коллекцию Set (реализация HashSet) с типом элементов String.
+Добавить в неё 10 строк:
+арбуз
+банан
+вишня
+груша
+дыня
+ежевика
+женьшень
+земляника
+ирис
+картофель
+*/
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        Set<String> set = new HashSet<String>();
+        set.add("арбуз");
+        set.add("банан");
+        set.add("вишня");
+        set.add("груша");
+        set.add("дыня");
+        set.add("ежевика");
+        set.add("женьшень");
+        set.add("земляника");
+        set.add("ирис");
+        set.add("картофель");
+
+    for(String string : set) System.out.println(string);
+
+    }
+}
